5a0ba33539108bbc83f2de1f7a052fedcf315f3b
114122016 <114122016@nitt.edu>
2023-06-17 02:06:18 +0530

    day-4 completed

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,12 +1,55 @@
 import 'package:flutter/material.dart';
 
-class Loginpage extends StatelessWidget {
+class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
       child: Column(
         children: [
-          Image.asset("assets/images/loginpage.pnb"),
+          Image.asset(
+            "assets/images/loginpage.png.png",
+            fit: BoxFit.contain,
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          Text(
+            "welcome to our page",
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                      hintText: "Enter username", labelText: "Username"),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      hintText: "Enter password", labelText: "password"),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    print("hi ayushman");
+                  },
+                  child: Text("Login"),
+                  style: TextButton.styleFrom(),
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );
